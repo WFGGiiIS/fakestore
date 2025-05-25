c0598191bc8322a2304b474fd5d3e37b90bb439f
WFGGiiIS <daniildzefir@example.com>
2025-05-25 17:51:58 +0200

    changed to PLN

--- fakestore/src/main.js
+++ fakestore/src/main.js
@@ -307,7 +307,7 @@ async function renderProducts() {
           />
           <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">$${
+            <h6 class="card-subtitle mb-2 text-body-secondary">zł${
               product.price
             }</h6>
             <p class="card-text">
